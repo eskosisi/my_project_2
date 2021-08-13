ca7901375ad854bdb3fcde7d840bb8941b10a029
eskosisi <eskoesko96>
2021-08-13 20:31:49 +0400

    second__update

---
+++ box.js
@@ -0,0 +1,9 @@
+let hiddenButton = document.querySelector('phone-horizontal__screen')
+
+function ChangeScreen() {
+    document.removeClass('.phone-horizontal__screen'])
+}
+
+phone-horizontal__screen.onclick = function() {
+    
+}
